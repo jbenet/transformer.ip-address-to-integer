2f56063efd2d24ba80982a3f82ce4f1f83734aec
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:06:09 -0700

    new version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var transformer = require('dat-transformer');
-var tIpAddress = transformer('ip-address');
-var tInteger = transformer('integer');
+var Conversion = require('transformer-conversion');
+var tIpAddress = require('transformer.ip-address');
+var tInteger = require('transformer.integer');
 // require any other modules you may need here.
 var ip = require('ip');
 
-module.exports = transformer.Conversion(tIpAddress, tInteger, convert);
+module.exports = Conversion(tIpAddress, tInteger, convert);
 
 function convert(input) {
   return ip.toLong(input);
